42746ece27bc38d7b15207b1bf093ac89c45b6e6
richo225 <richo225@gmail.com>
2016-11-24 15:51:43 +0000

    Bot repeats text upon message

--- telebot.js
+++ telebot.js
@@ -2,6 +2,13 @@ const dotenv = require("dotenv");
 dotenv.load();
 
 const TeleBot = require("telebot");
-const bot = new TeleBot("TELEGRAM_API_KEY");
+const bot = new TeleBot(process.env.TELEGRAM_API_KEY);
+
+// On every text message
+bot.on("text", msg => {
+  let id = msg.from.id;
+  let text = msg.text;
+  return bot.sendMessage(id, `You said: ${ text }`);
+});
 
 bot.connect();
